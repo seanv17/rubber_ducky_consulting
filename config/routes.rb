4a1a3277c7a28d019ae3e800412faf4f20d1ad8d
seanvsville <seanvanho@gmail.com>
2016-05-29 12:26:15 -0700

    created home controller, set as index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registration: "registration" }
 
-  root to: 'products#show'
+  root to: 'home#home'
 
   resources :products
   resources :charges
