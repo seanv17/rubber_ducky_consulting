c88c1ca6b0986c8385ce22c0e560fb2fef740cd4
seanvsville <seanvanho@gmail.com>
2016-05-30 11:58:37 -0700

    invoice_create show working

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -9,9 +9,14 @@ class InvoicesController < ApplicationController
   end
 
   def new
+    @invoice = Invoice.new
   end
 
   def create
+    invoice = Invoice.new(invoice_params)
+    if invoice.save
+      redirect_to invoice_path(invoice)
+    end
   end
 
   def edit
@@ -21,6 +26,12 @@ class InvoicesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:client_name, :date, :project_name, :project_summary, :amount_due)
   end
 
 end
