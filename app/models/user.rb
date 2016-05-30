6320cdeab9fd05770ce593b0326396a5f6098d7b
seanvsville <seanvanho@gmail.com>
2016-05-29 19:57:05 -0700

    set enum roles, default role for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  # set user enum role
+  enum role: {admin: 0, client: 1}
+
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :client
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
