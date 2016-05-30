41cc70f358051a70429589ce4d4c3b3490ec8a84
seanvsville <seanvanho@gmail.com>
2016-05-29 20:49:42 -0700

    finished seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,5 @@ admin_user   = User.create({ first_name: "Reggie", last_name: "Thompson", compan
 mattel_sarah_lewis = User.create({ first_name: "Sarah", last_name: "Lewis", company: "Mattel", role: 1, email: "sarah@gmail.com", password: "12345678" })
 nbc_george_vandalay = User.create({ first_name: "George", last_name: "Vandalay", company: "NBC", role: 1, email: "george@gmail.com", password: "12345678" })
 
-Invoice.create({ client_name: "Mattel", project_name: "iOS action_figure app", project_summary: "Become your favorite superheroes and villians, fight other players with blocky movement, and unlock new characters.", amount_due: 2999.99, date: 2014-10-12, user_id: mattel_sarah_lewis.id })
-Invoice.create({ client_name: "NBC", project_name: "Sketch comedy app", project_summary: "Challenge your friends and family to a game of improv and sketch comedy.", amount_due: 1599.99, date: 1997-2-12, user_id: nbc_george_vandalay.id })
+Invoice.create({ client_name: "Mattel", project_name: "iOS action_figure app", project_summary: "Become your favorite superheroes and villians, fight other players with blocky movement, and unlock new characters.", amount_due: 2999.99, date: Date.parse('10/5/2012 10:47'), user_id: mattel_sarah_lewis.id })
+Invoice.create({ client_name: "NBC", project_name: "Sketch comedy app", project_summary: "Challenge your friends and family to a game of improv and sketch comedy.", amount_due: 1599.99, date: Date.parse('02/12/1997 3:25'), user_id: nbc_george_vandalay.id })
