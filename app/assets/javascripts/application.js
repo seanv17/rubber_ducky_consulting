a2712a65601bc56f43bef9bd46944a9dfed58583
seanvsville <seanvanho@gmail.com>
2016-05-31 10:10:37 -0700

    fixed datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,4 @@
 //= require jquery_ujs
 //= require_tree ./sitewide
 //= require ./home/home
+//= require ./invoices/invoices
---
+++ app/assets/javascripts/invoices/invoices.js
@@ -0,0 +1,13 @@
+// Materialize JS for Calendar and Select Form
+
+  $(document).ready(function() {
+    $('select').material_select();
+  });
+
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year
+    format: 'dd-mm-yyyy'
+  });
+  
+});
