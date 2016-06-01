1ada69cb364d79fcd4151997f17641c7c118aeee
seanvsville <seanvanho@gmail.com>
2016-06-01 16:14:02 -0700

    fixed datepicker

--- app/assets/javascripts/invoices/invoices.js
+++ app/assets/javascripts/invoices/invoices.js
@@ -5,8 +5,7 @@ $(document).ready(function() {
 
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year
-    format: 'dd-mm-yyyy'
+    selectYears: 15 // Creates a dropdown of 15 years to control year
   });
 
 });
