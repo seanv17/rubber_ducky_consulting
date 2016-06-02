60ec882f6cb963fd8475589dbcf1955dede78a3e
seanvsville <seanvanho@gmail.com>
2016-06-02 16:13:49 -0700

    typo fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ invoice1 = Invoice.new
 invoice1.client_name = "Mattel"
 invoice1.project_name = "iOS action_figure app"
 invoice1.project_summary = "Become your favorite superheroes and villians, fight other players with blocky movement, and unlock new characters"
-invoice1.ammount_due = 3999.99
+invoice1.amount_due = 3999.99
 invoice1.date = Date.parse('10/5/2012 10:47')
 invoice1.user = client1
 invoice1.save
