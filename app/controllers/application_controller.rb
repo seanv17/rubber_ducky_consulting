7b525702fc4877dbb50a4f08110ff4d76051116e
seanvsville <seanvanho@gmail.com>
2016-06-03 08:55:43 -0700

    set not_found action in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Catch all route redirect to 404
+  def not_found
+    render :file => "/public/404.html"
+  end
+
 end
