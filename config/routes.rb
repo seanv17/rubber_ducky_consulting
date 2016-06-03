f4bd1b6dab3304ffa9d79930cdcf84ac0264da76
seanvsville <seanvanho@gmail.com>
2016-06-03 08:21:58 -0700

    added TODO for routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  # TODO!!! Exclude routes not being used
   # Set routes for charges and invoices controllers
   resources :charges
   resources :invoices
